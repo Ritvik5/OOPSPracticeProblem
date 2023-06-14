5a78be727c2802e896f60a2690810ee5b4fa6204
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-14 20:45:37 +0530

    Welcome Message

---
+++ OOPSPracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPSPracticeProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Practice Problem!!!\n");
+        }
+    }
+}
